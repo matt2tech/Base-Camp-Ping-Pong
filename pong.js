4aceb066a95067e2de0082912ef6b6f464813dea
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-02 12:43:37 -0500

    Gatherd all the usernames and displayed them when a button is clicked.

--- pong.js
+++ pong.js
@@ -1,9 +1,11 @@
+var PAGE_DATA = {};
+
 function signUp() {
   btn = document.getElementById("signUpBtn");
-  btn.addEventListener("click", function() {
+  btn.addEventListener("submit", function() {
     username = document.getElementById("usernameSignUp").value;
     password = document.getElementById("passwordSignup").value;
-    repeat = document.getElementById('passwordRepeatSignUp').value
+    repeat = document.getElementById("passwordRepeatSignUp").value;
     postData("https://bcca-pingpong.herokuapp.com/api/register/", {
       username: username,
       password: password,
@@ -25,18 +27,46 @@ function postData(url = "", data = {}) {
 }
 
 function login() {
-    btn = document.getElementById("loginBtn");
-    btn.addEventListener("click", function() {
-      username = document.getElementById("usernameLogin").value;
-      password = document.getElementById("passwordLogin").value;
-      postData("https://bcca-pingpong.herokuapp.com/api/login/", {
-        username: username,
-        password: password
+  btn = document.getElementById("loginBtn");
+  btn.addEventListener("submit", function() {
+    username = document.getElementById("usernameLogin").value;
+    password = document.getElementById("passwordLogin").value;
+    postData("https://bcca-pingpong.herokuapp.com/api/login/", {
+      username: username,
+      password: password
+    })
+      .then(data => {
+        console.log(JSON.stringify(data));
+        PAGE_DATA.token = token;
       })
-        .then(data => console.log(JSON.stringify(data)))
-        .catch(error => console.error(error));
-    });
-  }
+      .catch(error => console.error(error));
+  });
+}
 
-signUp()
-login()
+function seeData(url = "") {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": `${PAGE_DATA.token}`
+    },
+  }).then(response => response.json());
+}
+
+function seeUsers() {
+  btn = document.getElementById('userBtn');
+  users = document.getElementById('userList')
+  btn.addEventListener('click', function() {
+    seeData("https://bcca-pingpong.herokuapp.com/api/users/")
+  .then(data => {
+    console.log(JSON.stringify(data));
+    data.forEach(user => {
+      users.innerText += `${user.username}\n`
+    });
+  })
+  })
+}
+
+signUp();
+login();
+seeUsers();
