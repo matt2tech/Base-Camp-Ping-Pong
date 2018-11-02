502adb632a203bc3a2d9110df81af1a24562641e
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-02 09:47:35 -0500

    Added functions that post data to signup a user

--- pong.js
+++ pong.js
@@ -1,8 +1,25 @@
-var fetching = fetch("https://bcca-pingpong.herokuapp.com/api/register/");
-
 function signUp() {
-  var submitBtn = document.querySelector("#signUpBtn");
-    submitBtn.addEventListener("click", function() {
-        fetching
+  btn = document.getElementById("signUpBtn");
+  btn.addEventListener("click", function() {
+    username = document.getElementById("usernameSignUp").value;
+    password = document.getElementById("passwordSignup").value;
+    postData("https://bcca-pingpong.herokuapp.com/api/register/", {
+      name: username,
+      pass: password
     })
+      .then(data => console.log(JSON.stringify(data)))
+      .catch(error => console.error(error));
+  });
 }
+
+function postData(url = "", data = {}) {
+  return fetch(url, {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(data)
+  }).then(response => response.json());
+}
+
+signUp()
